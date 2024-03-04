1960e46d9310667ee0db87a823f7e6634bf60790
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-03-04 08:58:31 +0545

    item remove button

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -10,10 +10,12 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  Button,
 } from "@edge-ui/react";
+import { MdDelete } from "react-icons/md";
 
 export default function Cart() {
-  const { cart } = useStore(); // access the cart from the store
+  const { cart, removeFromCart } = useStore(); // access the cart and removeFromCart from the store
 
   // Calculate the total price
   const totalPrice = cart.reduce(
@@ -30,6 +32,7 @@ export default function Cart() {
             <TableHead className="w-[100px]">ID</TableHead>
             <TableHead>Item name</TableHead>
             <TableHead>Quantity</TableHead>
+            <TableHead>Remove</TableHead>
             <TableHead className="text-right">Amount</TableHead>
           </TableRow>
         </TableHeader>
@@ -39,6 +42,11 @@ export default function Cart() {
               <TableCell className="font-medium">{item.product.id}</TableCell>
               <TableCell>{item.product.tittle}</TableCell>
               <TableCell>{item.quantity}</TableCell>
+              <TableCell>
+                <Button onClick={() => removeFromCart(item.product.id)}>
+                  <MdDelete />
+                </Button>
+              </TableCell>
               <TableCell className="text-right">
                 {item.product.price * item.quantity}.00
               </TableCell>
@@ -47,11 +55,15 @@ export default function Cart() {
         </TableBody>
         <TableFooter>
           <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell colSpan={4}>Total</TableCell>
             <TableCell className="text-right">Rs.{totalPrice}.00</TableCell>
           </TableRow>
         </TableFooter>
       </Table>
+
+      <div className="flex items-center justify-center py-5">
+        <Button>Confirm Order </Button>
+      </div>
     </div>
   );
 }
