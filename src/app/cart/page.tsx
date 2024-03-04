f630e43bf2a5b82da32d922beb78ced981c19fe9
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-03-04 07:49:05 +0545

    qty count cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,62 +1,57 @@
 "use client";
 import React from "react";
 import { useStore } from "@/lib/store"; // import the store
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@edge-ui/react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@edge-ui/react";
 
 export default function Cart() {
   const { cart } = useStore(); // access the cart from the store
 
   // Calculate the total price
-  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const totalPrice = cart.reduce(
+    (total, item) => total + item.product.price * item.quantity,
+    0
+  );
 
   return (
-    <div className="padx"><Table>
-    <TableCaption>A list of your recent invoices.</TableCaption>
-    <TableHeader>
-      <TableRow>
-        <TableHead className="w-[100px]">ID</TableHead>
-        <TableHead>Item name</TableHead>
-        <TableHead>Quantity</TableHead>
-        <TableHead className="text-right">Amount</TableHead>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      {cart.map((item) => (
-        <TableRow key={item.id}>
-        <TableCell className="font-medium">{item.id}</TableCell>
-        <TableCell>{item.tittle}</TableCell>
-        <TableCell>{item.quantity}</TableCell>
-        <TableCell className="text-right">${item.price}</TableCell>
-      </TableRow>
-      ))}
-    </TableBody><TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total</TableCell>
-              <TableCell className="text-right">${totalPrice}</TableCell>
+    <div className="padx">
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Item name</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {cart.map((item) => (
+            <TableRow key={item.product.id}>
+              <TableCell className="font-medium">{item.product.id}</TableCell>
+              <TableCell>{item.product.tittle}</TableCell>
+              <TableCell>{item.quantity}</TableCell>
+              <TableCell className="text-right">
+                ${item.product.price * item.quantity}
+              </TableCell>
             </TableRow>
-          </TableFooter>
-  </Table>
-  
-      {/* <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((item, index) => (
-            <tr key={index}>
-              <td>{item.tittle}</td>
-              <td>1</td>
-              <td>{item.price}</td>
-            </tr>
           ))}
-        </tbody>
-      </table>
-      <p>Total Price: {totalPrice}</p> */}
-
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">${totalPrice}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
     </div>
   );
 }
